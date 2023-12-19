36c4a452dd32f75c0b0372c14e6dfe3799b0f2c5
M-itti <mahdlinux@gmail.com>
2023-12-18 23:37:32 -0800

    trying to make db and auth work

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
-from flask import Flask
-from flask import render_template
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from auth import auth_bp  
+
+from models import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # SQLite database URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking, as it is unnecessary
+
 socketio = SocketIO(app)
 
+db.init_app(app)
+
+# Register the authentication blueprint with the app
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
 @app.route("/")
-def hello_world():    
+def index():
     return render_template('index.html')
 
+# Save it to the database
 @socketio.on('my event')
 def handle_message(data):
-    # save it to db
-        print('received message: ', data)
+    from models import Message
+    # Assuming 'user_id' is available in the 'data' dictionary
+    new_message = Message(text=data['message'], user_id=data['user_id'])
+    db.session.add(new_message)
+    db.session.commit()
+    print('received message: ', data)
 
 if __name__ == "__main__":
-        socketio.run(app)
+    # Create the database tables (if they don't exist) before running the app
+    db.create_all()
+    socketio.run(app)
 
---
+++ auth.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
+from models import User, db
+from flask_bcrypt import check_password_hash, generate_password_hash
+from flask import request 
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hashed_password = generate_password_hash(password).decode('utf-8')
+
+        # Save user to the database
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account created successfully!', 'success')
+        return redirect(url_for('auth.login'))
+
+    return render_template('register.html')
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            # Log the user in
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login unsuccessful. Please check your credentials.', 'danger')
+
+    return render_template('login.html')
+
+@auth_bp.route("/logout")
+def logout():
+    # Implement logout logic here
+    return "Logged out successfully"
+
