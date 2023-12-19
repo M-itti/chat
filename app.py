664365178d48b3068a5d6b7c5c5b1c719851ff72
M-itti <mahdlinux@gmail.com>
2023-12-19 13:49:06 -0800

    seperating js and html

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from auth import auth_bp  
+#from flask_migrate import Migrate
 
 from models import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # SQLite database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # SQLite database URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking, as it is unnecessary
 
 socketio = SocketIO(app)
 
+#migrate = Migrate(app, db)
 db.init_app(app)
 
 # Register the authentication blueprint with the app
@@ -25,6 +27,7 @@ def index():
 @socketio.on('my event')
 def handle_message(data):
     from models import Message
+    print(data)
     # Assuming 'user_id' is available in the 'data' dictionary
     new_message = Message(text=data['message'], user_id=data['user_id'])
     db.session.add(new_message)
@@ -33,6 +36,8 @@ def handle_message(data):
 
 if __name__ == "__main__":
     # Create the database tables (if they don't exist) before running the app
-    db.create_all()
+    print('test')
+    with app.app_context():
+        db.create_all()
     socketio.run(app)
 
--- static/script.js
+++ static/script.js
@@ -1,4 +1,32 @@
 var socket = io();
-    socket.on('connect', function() {
-        socket.emit('my event', {data: 'I\'m connected!'});
+
+socket.on('connect', function() {
+    var messageData = {
+        message: 'Hello, this is a message!',
+        user_id: '12345' // Replace this with the actual user ID
+    };
+    socket.emit('my event', messageData);
 });
+
+// Function to send a message
+function sendMessage() {
+    var messageInput = document.getElementById('message');
+    var message = messageInput.value;
+    messageInput.value = '';
+
+    if (message.trim() !== '') {
+        var chatBox = document.getElementById('chat-box');
+        var newMessage = document.createElement('p');
+        newMessage.textContent = message;
+        chatBox.appendChild(newMessage);
+
+        // Scroll chat box to bottom
+        chatBox.scrollTop = chatBox.scrollHeight;
+
+        // Send message to the server using Socket.IO
+        socket.emit('my event', { data: message });
+    }
+}
+
+// Attach the sendMessage function to the button click event
+document.getElementById('sendButton').addEventListener('click', sendMessage);
