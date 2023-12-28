f24d586f310ebf7ff4e5a771f347a0cef1a29b7f
M-itti <mahdlinux@gmail.com>
2023-12-27 22:04:10 -0800

    adding  auth and model

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
+from model import User
+from auth import auth
+from model import db, User
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'secret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'  # SQLite database file
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-# Define the Message model
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
-    message = db.Column(db.String(500))
+db.init_app(app)
 
 @app.route('/')
 def index():
@@ -25,20 +25,20 @@ def handle_new_message(data):
     message_content = data.get('message')
     
     # Create a new Message instance
-    new_message = Message(username=username, message=message_content)
+    new_message = User(username=username, message=message_content)
     
     # Add the message to the database
     db.session.add(new_message)
     db.session.commit()
     
     # Emit the updated messages to all clients
-    messages = Message.query.all()
+    messages = User.query.all()
     emit('update_messages', [{'username': msg.username, 'message': msg.message} for msg in messages], broadcast=True)
 
 @socketio.on('connect')
 def handle_connect():
     # Retrieve all messages from the database and emit them to the client
-    messages = Message.query.all()
+    messages = User.query.all()
     emit('update_messages', [{'username': msg.username, 'message': msg.message} for msg in messages])
 
 if __name__ == '__main__':
---
+++ auth.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
+from model import db, User
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('main.index'))
+
+        flash('Invalid username or password', 'error')
+
+    return render_template('login.html')
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            flash('Username already exists', 'error')
+        elif password != confirm_password:
+            flash('Passwords do not match', 'error')
+        else:
+            hashed_password = generate_password_hash(password)
+            new_user = User(username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created successfully! Please log in.', 'success')
+            return redirect(url_for('auth.login'))
+
+    return render_template('register.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out successfully!', 'success')
+    return redirect(url_for('main.index'))
+
